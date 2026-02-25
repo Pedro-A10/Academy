8434d2d618dd151721fd8d72ece98680b44063ce
Pedro-A10 <pedroaugusto732@gmail.com>
2026-02-25 16:38:19 -0300

    Create: Classe Aluno criada como entidade JPA.

---
+++ src/main/java/com/Android/Academia/model/Aluno.java
@@ -0,0 +1,22 @@
+package com.Android.Academia.model;
+
+import com.Android.Academia.model.exercicio.ListaExercicio;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity(name = "alunos")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Aluno extends User{
+
+    @OneToMany
+    private List<ListaExercicio> listaDeTreino = new ArrayList<>();
+}
