a16280a54c2e995c267cd4f9b7afe7883d8c9f78
Pedro-A10 <pedroaugusto732@gmail.com>
2026-02-25 19:48:39 -0300

    Create: Interface de Funcionário para repositório criada.

---
+++ src/main/java/com/Android/Academia/repository/FuncionarioRepo.java
@@ -0,0 +1,32 @@
+package com.Android.Academia.repository;
+
+import com.Android.Academia.enums.Funcao;
+import com.Android.Academia.model.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface FuncionarioRepo extends JpaRepository<Funcionario, Long> {
+
+    Optional<Funcionario> findFuncionariosById(Long id);
+
+    Optional<Funcionario> findFuncionariosByCPF(String cpf);
+
+    Optional<Funcionario> findFuncionariosByNome(String nome);
+
+    Optional<Funcionario> findFuncionarioByTelefone(String telefone);
+
+    Optional<Funcionario> findAllByNome(String nome);
+
+    Optional<Funcionario> findAllByCPF(String cpf);
+
+    //Procura via Enum
+    Optional<Funcionario> findAllByFuncao(Funcao funcao);
+    
+    boolean existsFuncionarioByNome(String nome);
+    boolean existsFuncionarioByEmail(String email);
+    boolean existsFuncionarioByCPF(String cpf);
+
+}
