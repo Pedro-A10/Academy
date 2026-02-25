92ce38339fb1bf519b0ac4ac369e1fdf34baa4f7
Pedro-A10 <pedroaugusto732@gmail.com>
2026-02-25 14:13:49 -0300

    Create: Classe User criada.

---
+++ src/main/java/com/Android/Academia/model/User.java
@@ -0,0 +1,57 @@
+package com.Android.Academia.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "users")
+public class User {
+    //O sqlite com o jpa não tem um suporte bom
+
+    //variáveis
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message = "Nome necessário")
+    @Pattern(regexp = "^[A-Za-zÀ-ú]+(\\s[A-Za-zÀ-ú]+)+$",
+            message = "Por favor colocar o nome inteiro.")
+    @Column(unique = true)
+    private String nome;
+
+    @NotBlank(message = "Endereço necessário")
+    private String endereco;
+
+    @NotBlank(message = "Telefone necessário")
+    private String telefone;
+
+    @NotBlank(message = "Data de nascimento necessária")
+    @Column(unique = true)
+    private LocalDate nascimento;
+
+    @NotBlank(message = "CPF é necessário")
+    @CPF(message = "CPF invalido")
+    @Column(unique = true)
+    private String CPF;
+
+    @NotBlank(message = "O Email é necessário")
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotEmpty(message = "A senha é necessária")
+    @Size(min = 8, max = 100, message = "A senha não pode ter menos de 8 letras!")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            message = "A senha deve conter números e símbolos especiais.")
+    @Column(unique = true)
+    private String password;
+}
