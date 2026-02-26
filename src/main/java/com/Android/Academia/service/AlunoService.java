939840d6ea4a865742059451e28f6cf511abe0fb
Pedro-A10 <pedroaugusto732@gmail.com>
2026-02-26 14:32:00 -0300

    Create: Classe AlunoService concluída sem o uso de mapper ou dto.

---
+++ src/main/java/com/Android/Academia/service/AlunoService.java
@@ -0,0 +1,90 @@
+package com.Android.Academia.service;
+
+import com.Android.Academia.enums.Funcao;
+import com.Android.Academia.enums.Role;
+import com.Android.Academia.model.Aluno;
+import com.Android.Academia.model.Funcionario;
+import com.Android.Academia.model.User;
+import com.Android.Academia.repository.AlunoRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AlunoService {
+
+    @Autowired
+    AlunoRepo alunoR;
+
+    /*TODO: implementar uma mapper e uma dto de alunos
+        para substituir os métodos que utilizam o AlunoRepo diretamente.
+        Criar exceptions personalizadas para os métodos que podem gerar valores incorretos
+     */
+
+    //hashing de senhas usar só depois de criar os dto e mapper
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    //Metodo auxiliar para permissões de CRUD
+    private void verificarPermissoes(User permitido) {
+        //Permissão para staff
+        if (permitido.getRole() == Role.STAFF) {
+            return;
+        }
+        //permissão para recpção
+        if (permitido instanceof Funcionario funcionario &&
+                funcionario.getFuncao() == Funcao.RECEPCAO) {
+            return;
+        }
+        throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Sem permissão para gerenciar aluno");
+    }
+
+    //Get
+    public Optional<Aluno> findAlunosById(Long id) {
+        return alunoR.findAlunosById(id);
+    }
+
+    public Optional<Aluno> findAlunosByCPF(String cpf) {
+        return alunoR.findAlunosByCPF(cpf);
+    }
+
+    public Optional<Aluno> findAlunosByEmail(String email) {
+        return alunoR.findAlunosByEmail(email);
+    }
+
+    public List<Aluno> findAlunosByNome(String nome) {
+        return alunoR.findAlunosByNome(nome);
+    }
+
+    public List<Aluno> findAlunosByTelefone(String telefone) {
+        return alunoR.findAlunosByTelefone(telefone);
+    }
+
+    //Booleans
+    public boolean existsAlunosByEmail(String email) {
+        return alunoR.existsAlunosByEmail(email);
+    }
+
+    public boolean existsAlunosByCPF(String cpf) {
+        return alunoR.existsAlunosByCPF(cpf);
+    }
+
+    //Operações de CRUD (CREATE / DELETE)
+    public void criarAluno(User permitido, Aluno novoAluno) {
+        verificarPermissoes(permitido);
+        alunoR.save(novoAluno);
+    }
+
+    public void deletarAlunoById(User permitido, Long id) {
+        verificarPermissoes(permitido);
+        Aluno aluno = alunoR.findById(id)
+                .orElseThrow(() -> new ResponseStatusException
+                        (HttpStatus.NOT_FOUND, "Aluno não encontrado com id: " + id));
+        alunoR.delete(aluno);
+    }
+}
